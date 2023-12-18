f5d6819667ed191dc20675b2a15378fa8fd341f6
tomislav-zupanic <149964059+tomislav-zupanic@users.noreply.github.com>
2023-12-18 18:33:54 +0100

    csharpNovo

---
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,16 @@
+﻿//Console.WriteLine(7);
+//Console.WriteLine(true);
+//Console.WriteLine("Prvi");
+//Console.WriteLine("Drugi");
+//Console.WriteLine(3.14);
+
+//Console.Write("Unesi ime: ");
+//string ime =  Console.ReadLine();
+
+//Console.WriteLine("Unijeli ste " + ime);
+
+
+Console.Write("Unesi visinu u centimetrima: ");
+int Visina = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Visoki ste " + Visina / 100 + "metara");
